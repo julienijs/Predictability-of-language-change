38046febe4d6ef64d83afbcb5d2f9b11583f5ec1
Julie Nijs <107923146+nysju@users.noreply.github.com>
2023-01-18 11:50:25 +0100

    Update How_Predictable_Is_Language_Change.R

--- How_Predictable_Is_Language_Change.R
+++ How_Predictable_Is_Language_Change.R
@@ -12,7 +12,7 @@ library(ggplot2)
 library(tools)
 library(graphics)
 
-######PART 1: Processing the datasets######
+######Processing the datasets######
 
 # compile a list of .xlsx files (in working directory) that need to be processed:
 filenames <- list.files(getwd(), pattern="xlsx*", full.names=TRUE)
@@ -223,63 +223,4 @@ for (file in filenames) {
 # save finaldf to .xlsx file:
 write.xlsx(finaldf, "residuals_analysis.xlsx", sheetName = "Sheet1", 
            col.names = TRUE, row.names = TRUE, append = FALSE)
-
-######PART 2: Analysis of the residuals######
-
-# read residuals_analysis.xlsx:
-ResAnalysis <- read_xlsx("residuals_analysis.xlsx", col_names = TRUE)
-
-# add a column "significance", where "no" is added for those datasets were not significant in the previous analysis:
-ResAnalysis <- cbind(ResAnalysis, Significance=NA)
-
-file1 = "The dat wat shift in the superlative"
-file2 = "The rise of experiencer-subject construal with psych verbs"
-for (row in 1:nrow(ResAnalysis)) {
-  if (ResAnalysis$File[row] == file1 || ResAnalysis$File[row] == file2) {
-    ResAnalysis$Significance[row] = "no"
-  } else {
-    ResAnalysis$Significance[row] = "yes"
-  }
-}
-
-ResAnalysis <- droplevels(ResAnalysis[!ResAnalysis$Significance=="no",]) #throw out datasets that were not significant
-
-
-ResAnalysis$Change_Type <- as.factor(ResAnalysis$Change_Type)
-ResAnalysis$Curve_Type <- relevel(as.factor(ResAnalysis$Curve_Type), ref="full")
-ResAnalysis$Window <- relevel(as.factor(ResAnalysis$Windowposition), ref="middle omitted")
-
-# boxplots:
-boxplot(ResAnalysis$Standardized_residual ~ ResAnalysis$Windowposition)
-boxplot(ResAnalysis$Standardized_residual ~ ResAnalysis$Change_Type)
-boxplot(ResAnalysis$Standardized_residual ~ ResAnalysis$Curve_Type)
-
-boxplot(ResAnalysis$Raw_Residual ~ ResAnalysis$Windowposition)
-boxplot(ResAnalysis$Raw_Residual ~ ResAnalysis$Change_Type)
-boxplot(ResAnalysis$Raw_Residual ~ ResAnalysis$Curve_Type)
-
-# linear models:
-summary(w <- lm(Standardized_residual ~ Window, 
-                data=ResAnalysis))
-summary(w <- lm(Standardized_residual ~ Curve_Type, 
-                data=ResAnalysis))
-summary(w <- lm(Standardized_residual ~ Change_Type, 
-                data=ResAnalysis))
-summary(w <- lm(Standardized_residual ~ Window*Curve_Type + Change_Type, 
-                data=ResAnalysis))
-summary(w <- lm(Standardized_residual ~ Window*Curve_Type, 
-                data=ResAnalysis)) # --> most significant effects
-plot(allEffects(w), 
-     ylab="Standardized residuals", 
-     xlab="Part of the curve",
-     main="Interaction effect for window position and part of the curve")
-
-# histogram:
-
-hist(ResAnalysis$Standardized_residual, 
-     main="Histogram of the standardized residuals (aggregated)",
-     xlab="Standardized residuals",
-     ylim=c(0,200),
-     labels=TRUE)
-
 
